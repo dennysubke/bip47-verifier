d767d009512522818552f1e0c5918a8e2d93b6ad
Denny Subke <155585746+dennysubke@users.noreply.github.com>
2025-05-28 10:11:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+// vite.config.ts
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: "bip47-verifier",
+    base: "/bip47-verifier/",
     plugins: [
         react(),
     ],
